64c038387b92ec30e3c0f46b26ae1521d2b80589
Ben Gottlieb <ben@standalone.com>
2021-11-07 04:08:44 -0600

    Initial Commit

---
+++ Bulletin/BulletinApp.swift
@@ -0,0 +1,17 @@
+//
+//  BulletinApp.swift
+//  Bulletin
+//
+//  Created by Ben Gottlieb on 11/7/21.
+//
+
+import SwiftUI
+
+@main
+struct BulletinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
