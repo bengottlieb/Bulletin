22d63a8f1eea99e35db1f0676997ccc0bdb9b918
Ben Gottlieb <ben@standalone.com>
2021-11-08 05:16:02 -0600

    added message passing

--- Sources/Bulletin/Bulletin.swift
+++ Sources/Bulletin/Bulletin.swift
@@ -5,20 +5,20 @@
 //  Created by Ben Gottlieb on 11/7/21.
 //
 
-import Foundation
 import WebKit
-import SwiftUI
+import Suite
 
 public class Bulletin: NSObject, ObservableObject, WKNavigationDelegate {
 	let url: URL?
 	let html: String?
-	let webView: WKWebView
+	var webView: WKWebView!
 	var currentNavigation: WKNavigation?
-    @Published var hasLoadedOnce = false
-	@Published var loadingState: LoadingState = .notLoaded
-
-	enum LoadingState: Equatable { case notLoaded, inProgress, loaded, failed(Error)
-		static func ==(lhs: LoadingState, rhs: LoadingState) -> Bool {
+	@Published public var hasLoadedOnce = false
+	@Published public var loadingState: LoadingState = .notLoaded
+	public var message = CurrentValueSubject<String?, Never>(nil)
+	
+	public enum LoadingState: Equatable { case notLoaded, inProgress, loaded, failed(Error)
+		public static func ==(lhs: LoadingState, rhs: LoadingState) -> Bool {
 			switch (lhs, rhs) {
 			case (.notLoaded, .notLoaded): return true
 			case (.loaded, .loaded): return true
@@ -27,24 +27,27 @@ public class Bulletin: NSObject, ObservableObject, WKNavigationDelegate {
 			}
 		}
 	}
-
-	public init(url: URL? = nil, html: String? = nil) {
-		self.url = url
+	
+	public init(url: URL? = nil, html: String? = nil, file: String? = nil) {
+		if let filename = file {
+			self.url = Bundle.main.url(forResource: filename, withExtension: nil)
+		} else {
+			self.url = url
+		}
 		self.html = html
 		
-		let configuration = WKWebViewConfiguration()
-        webView = WKWebView(frame: UIScreen.main.bounds, configuration: configuration)
-		
 		super.init()
+		webView = WKWebView(frame: UIScreen.main.bounds, configuration: configuration)
+		
 		webView.navigationDelegate = self
 		currentNavigation = webView.load(self)
 	}
-
+	
 	public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
 		withAnimation {
-            hasLoadedOnce = true
-            loadingState = .loaded
-        }
+			hasLoadedOnce = true
+			loadingState = .loaded
+		}
 	}
 	
 	public func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
@@ -54,6 +57,23 @@ public class Bulletin: NSObject, ObservableObject, WKNavigationDelegate {
 	public func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
 		withAnimation { loadingState = .inProgress }
 	}
-
+	
+	var configuration: WKWebViewConfiguration {
+		let config = WKWebViewConfiguration()
+		config.userContentController = WKUserContentController()
+		config.userContentController.add(self, name: "postBulletinEvent")
+		
+		let postScript = WKUserScript(source: "function postBulletinEvent(event) { window.webkit.messageHandlers.postBulletinEvent.postMessage(event); }", injectionTime: .atDocumentEnd, forMainFrameOnly: false)
+		config.userContentController.addUserScript(postScript)
+		
+		return config
+	}
+	
 	static let sample = Bulletin(url: URL(string: "https://daringfireball.net"))
+}
+
+extension Bulletin: WKScriptMessageHandler {
+	public func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+		self.message.send(message.body as? String)
+	}
 }
--- Test Harness/App/ContentView.swift
+++ Test Harness/App/ContentView.swift
@@ -7,45 +7,46 @@
 
 import SwiftUI
 
-let staticBulletin = Bulletin(url: URL(string: "https://daringfireball.net"))
-
 struct ContentView: View {
-    @ObservedObject var bulletin = staticBulletin
-    @State var isVisible = false
-    
-    var body: some View {
-		 ZStack() {
-             Rectangle()
-             
-			 Image("sample_background")
-				 .resizable()
-				 .aspectRatio(contentMode: .fill)
-				 .edgesIgnoringSafeArea(.all)
-				 .layoutPriority(-1)
-             
-             VStack() {
-                 Button("Show") {
-                     withAnimation { isVisible.toggle() }
-                 }
-                 .foregroundColor(.white)
-                 .font(.system(size: 40))
-                 .disabled(bulletin.loadingState != .loaded)
-                 
-                 if bulletin.webView.isLoading {
-                     ProgressView()
-                         .frame(width: 60)
-                 }
-             }
-			 
-             BulletinView(bulletin: bulletin)
-                 .padding(20)
-                 .placement(.middle(0.8), for: bulletin, isVisible: $isVisible, blockingColor: .green.opacity(0.25))
-        }
-    }
+	@StateObject var bulletin = Bulletin(file: "bulletin.html")
+	@State var isVisible = true
+	
+	var body: some View {
+		ZStack() {
+			Rectangle()
+			
+			Image("sample_background")
+				.resizable()
+				.aspectRatio(contentMode: .fill)
+				.edgesIgnoringSafeArea(.all)
+				.layoutPriority(-1)
+			
+			VStack() {
+				Button(isVisible ? "Hide" : "Show") {
+					withAnimation { isVisible.toggle() }
+				}
+				.foregroundColor(.white)
+				.font(.system(size: 40))
+				.disabled(bulletin.loadingState != .loaded)
+				
+				if bulletin.webView.isLoading {
+					ProgressView()
+						.frame(width: 60)
+				}
+			}
+			
+			BulletinView(bulletin: bulletin)
+				.padding(20)
+				.placement(.middle(0.8), for: bulletin, isVisible: $isVisible)
+		}
+		.onReceive(bulletin.message) { message in
+			if message == "done" { withAnimation { isVisible = false } }
+		}
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
